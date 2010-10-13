f1281776125570ec939268d363810b4f0edb93b0
Tim Harrison <tharris4@wested.org>
2010-10-13 08:31:49 -0700

    Add a way to disable transactional factories for a single test.

--- lib/transactional_factories/test_case.rb
+++ lib/transactional_factories/test_case.rb
@@ -1,21 +1,25 @@
 module TransactionalFactories
   module ClassMethods
     def suite_with_transactions
-      method_names = public_instance_methods(true)
-      tests = method_names.delete_if {|method_name| method_name !~ /^test./}
-      suite = TransactionalFactories::TestSuite.new(name)
-      tests.sort.each do
-        |test|
-        catch(:invalid_test) do
-          suite << new(test)
+      if use_transactional_factories.is_a?(FalseClass)
+        suite_without_transactions
+      else
+        method_names = public_instance_methods(true)
+        tests = method_names.delete_if {|method_name| method_name !~ /^test./}
+        suite = TransactionalFactories::TestSuite.new(name)
+        tests.sort.each do
+          |test|
+          catch(:invalid_test) do
+            suite << new(test)
+          end
         end
-      end
-      if (suite.empty?)
-        catch(:invalid_test) do
-          suite << new("default_test")
+        if (suite.empty?)
+          catch(:invalid_test) do
+            suite << new("default_test")
+          end
         end
+        return suite
       end
-      return suite
     end
   end
   
@@ -23,6 +27,7 @@ module TransactionalFactories
     class << klass
       include ClassMethods
       alias_method_chain 'suite', 'transactions'
+      cattr_accessor :use_transactional_factories
     end
   end
 end
